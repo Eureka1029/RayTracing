8bec6b7facd0e41b0f9e3de504602b63ea174a21
Eureka <1285523226@qq.com>
2026-04-20 16:35:58 +0800

    定义视口和宽高比

--- main.cpp
+++ main.cpp
@@ -6,9 +6,18 @@
 int main() {
 
     // Image
+    auto aspect_ratio = 16.0/ 9.0; //设置宽高比,这样不需要同时修改高和宽来修改画面大小
+    int image_width = 400;
 
-    int image_width = 256;
-    int image_height = 256;
+    //计算高,确保他至少大于等于1.
+    int image_height = int(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    //设置虚拟视口
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (double(image_width)/image_height);
+    
+
 
     // Render
 
