ebc53160e072e64a5151e2824f590cde57cbaeeb
Eureka <1285523226@qq.com>
2026-04-21 15:22:25 +0800

    根据法向量着色的球体

--- main.cpp
+++ main.cpp
@@ -3,20 +3,28 @@
 #include "vec3.h"
 #include "ray.h"
 #include "color.h"
-bool hit_sphere(const point3& center, double radius, const ray& r){
+double hit_sphere(const point3& center, double radius, const ray& r){
     vec3 oc = center - r.origin(); 
     auto a = dot(r.direction(), r.direction());
     auto b = -2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius*radius;
     auto discriminant = b*b - 4*a*c; //判别式
-    return discriminant >= 0;
+   
+    if(discriminant < 0){
+        return -1.0;
+    }else {
+        return (-b - std::sqrt(discriminant)) / (2.0 * a); //返回t
+    }
 
 }
 
 
 color ray_color(const ray& r){
-    if(hit_sphere(point3(0,0,-1),0.5,r))
-        return color(1, 0, 0);
+    auto t = hit_sphere(point3(0,0,-1),0.5,r);
+    if(t > 0.0){
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1)); //打到球面上的点的法线,并归一化
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1); //将(x,y,z)映射到(red,green,blue) 
+    }
 
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
