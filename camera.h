d36e674b73a36065bba4e42f32c507a04bc2b552
Eureka <1285523226@qq.com>
2026-04-27 16:00:36 +0800

    重构代码,添加注释

--- camera.h
+++ camera.h
@@ -1,22 +1,21 @@
+
 #ifndef CAMERA_H
 #define CAMERA_H
 
-#include "color.h"
 #include "hittable.h"
-#include "hittable_list.h"
-#include "sphere.h"
 
 
 class camera {
 public:
-    double aspect_ratio = 16.0/ 9.0; //设置宽高比,这样不需要同时修改高和宽来修改画面大小
-    int image_width = 100; //宽
-    camera() {};
+    double aspect_ratio = 1.0;  // 图像宽高比（宽 / 高）
+    int    image_width  = 100;  // 输出图像宽度（像素）
 
     void render(const hittable& world) {
         initialize();
+
         std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
+        // 逐行逐列遍历像素并写出颜色。
         for (int j = 0; j < image_height; j++) {
             std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
             for (int i = 0; i < image_width; i++) {
@@ -33,61 +32,52 @@ public:
     }
 
 private:
-    int image_height; //渲染图像的高
-    point3 center; //相机位置
-    point3 pixel00_loc; //左上角像素的位置
-    vec3 pixel_delta_u; //
-    vec3 pixel_delta_v;
-
+    int    image_height;   // 输出图像高度（像素）
+    point3 center;         // 相机中心
+    point3 pixel00_loc;    // 左上角第一个像素中心位置
+    vec3   pixel_delta_u;  // 向右一个像素的位移
+    vec3   pixel_delta_v;  // 向下一个像素的位移
 
     void initialize() {
-        // Image
-        
-
-        //计算高,确保他至少大于等于1.
-        int image_height = int(image_width / aspect_ratio);
+        // 根据宽高比计算高度，并保证至少为 1。
+        image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
 
-        // 世界
-        hittable_list world;
+        // 相机位于原点。
+        center = point3(0, 0, 0);
 
-        world.add(make_shared<sphere>(point3(0,0,-1), 0.5)); 
-        world.add(make_shared<sphere>(point3(0,-100.5,-1),100));
-
-
-        //相机
-        auto focal_length = 1.0; //焦距
+        // 计算视口尺寸。
+        auto focal_length = 1.0;
         auto viewport_height = 2.0;
         auto viewport_width = viewport_height * (double(image_width)/image_height);
-        auto camera_center = point3(0, 0, 0);
 
-        //视口的向量
+        // 计算视口水平和竖直方向边向量。
         auto viewport_u = vec3(viewport_width, 0, 0);
         auto viewport_v = vec3(0, -viewport_height, 0);
 
-        //遍历一个像素在视口上的水平和垂直方向的位移
-        auto pixel_delta_u = viewport_u / image_width;
-        auto pixel_delta_v = viewport_v / image_height;
+        // 计算像素间在水平和竖直方向的步进向量。
+        pixel_delta_u = viewport_u / image_width;
+        pixel_delta_v = viewport_v / image_height;
 
-        //计算左上角的像素位置
-        auto viewport_upper_left = camera_center
-                                - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2; //视口左上顶点
-        auto piexl00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v); //第一个像素位置
-
+        // 计算视口左上角以及首像素中心位置。
+        auto viewport_upper_left =
+            center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+        pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 
     color ray_color(const ray& r, const hittable& world) const {
         hit_record rec;
 
-        if (world.hit(r, interval(0, infinity), rec)){
-            return 0.5 * (rec.normal + color(1, 1, 1));
+        // 命中物体时根据法线返回可视化颜色。
+        if (world.hit(r, interval(0, infinity), rec)) {
+            return 0.5 * (rec.normal + color(1,1,1));
         }
 
+        // 未命中时返回天空渐变背景。
         vec3 unit_direction = unit_vector(r.direction());
-        auto a = 0.5 * (unit_direction.y() + 1.0);
-        return (1.0 - a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+        auto a = 0.5*(unit_direction.y() + 1.0);
+        return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
     }
 };
 
 #endif
-
