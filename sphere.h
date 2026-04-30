9ef03875f1da033d2a3678dcd43b310a282a26d4
Eureka <1285523226@qq.com>
2026-04-30 10:17:54 +0800

    修改bug

--- sphere.h
+++ sphere.h
@@ -9,8 +9,7 @@
 class sphere : public hittable {
 public:
     sphere(const point3& center, double radius, std::shared_ptr<material> mat) 
-    : center(center), radius(std::fmax(0, radius)) {
-        // 需要初始化材质指针mat
+    : center(center), radius(std::fmax(0, radius)), mat(mat) {
     };
 
     bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
